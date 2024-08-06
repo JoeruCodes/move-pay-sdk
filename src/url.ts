989869e9ccd1e4684aeb70852c761ef07f759de9
Yeah_i_code <joymedi8143@gmail.com>
2024-08-06 06:15:22 +0530

    added decrypt from search params

--- src/url.ts
+++ src/url.ts
@@ -8,30 +8,34 @@ export function decryptURL(url: URL, SECRET_KEY: Uint8Array): RawJsonPayload {
   const nonce = urlParams.get("nonce");
   const encrypted = urlParams.get("data");
   if (nonce && encrypted) {
-    const encryptedData = {
-      nonce: nonce,
-      encrypted: encrypted,
-    };
-    const decryptedPayload = decryptPayload(encryptedData, SECRET_KEY);
-
-    const ret: RawJsonPayload = JSON.parse(decryptedPayload);
-    const newArgs = [];
-    for (const i of ret.data.functionArguments) {
-      if (i !== null && typeof i === "object" && !Array.isArray(i)) {
-        // Convert object with numeric keys back to Uint8Array
-        const dataObject = (i as any).data;
-        const uint8Array = new Uint8Array(
-          Object.values(dataObject),
-        ) as AccountAddressInput;
-        const s = AccountAddress.from(uint8Array);
-        newArgs.push(s);
-      } else {
-        newArgs.push(i);
-      }
-    }
-    ret.data.functionArguments = newArgs;
-    return ret;
+    return decryptSearchParams(nonce, encrypted, SECRET_KEY);
   }
 
   throw Error("Couldn't parse URL");
+}
+
+export function decryptSearchParams(nonce: string, data: string, SECRET_KEY: Uint8Array): RawJsonPayload{
+  const encryptedData = {
+    nonce: nonce,
+    encrypted: data,
+  };
+  const decryptedPayload = decryptPayload(encryptedData, SECRET_KEY);
+
+  const ret: RawJsonPayload = JSON.parse(decryptedPayload);
+  const newArgs = [];
+  for (const i of ret.data.functionArguments) {
+    if (i !== null && typeof i === "object" && !Array.isArray(i)) {
+      // Convert object with numeric keys back to Uint8Array
+      const dataObject = (i as any).data;
+      const uint8Array = new Uint8Array(
+        Object.values(dataObject),
+      ) as AccountAddressInput;
+      const s = AccountAddress.from(uint8Array);
+      newArgs.push(s);
+    } else {
+      newArgs.push(i);
+    }
+  }
+  ret.data.functionArguments = newArgs;
+  return ret;
 }
